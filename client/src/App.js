85a718d52e1d5243837b7d0b8531df78ff807132
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-11 20:17:05 +0200

    Add styles, grid and dummy posts.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import NavBar from './components/NavBar'
+import BlogPost from './components/BlogPost'
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import './App.css';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
 const categories = [
   { title: 'Category 1', url: '#' },
@@ -8,11 +19,71 @@ const categories = [
   { title: 'Category 3', url: '#' },
 ];
 
-export default function App() {
-  return (
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Mar 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Mar 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Featured post',
+    date: 'Mar 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Mar 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Featured post',
+    date: 'Mar 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Mar 11',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
 
-    <div>
+];
+
+export default function App() {
+
+  const classes = useStyles();
+  return (
+    <CssBaseline>
+    <Container maxWidth="lg">
       <NavBar title="FlotsamBlog" categories={categories}/>
-    </div>
+      <Grid container className={classes.root} spacing={1}>
+            {featuredPosts.map(post => (
+              <BlogPost key={post.title} post={post} />
+            ))}
+      </Grid>
+    </Container>
+    </CssBaseline>
   );
 }
