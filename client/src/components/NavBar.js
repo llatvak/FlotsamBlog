a5bdf3866efc587d5097234b63c66ff72fe09e1c
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-11 20:14:51 +0200

    Add styles.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles({
+    appbar: {
+        padding: 10,
+      },
+  });
 
 const NavBar = (props) => {
 
     const { categories, title } = props;
+    const classes = useStyles();
 
     return (
-        <div>
-        <AppBar position="static">
+        <AppBar className={classes.appbar} position="static">
             <Toolbar>
                 <Typography variant="title" color="inherit">
                 {title}
                 </Typography>
             </Toolbar>
         </AppBar>
-        </div>
     )
 }
 export default NavBar;
