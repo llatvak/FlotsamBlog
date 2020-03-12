79e2db734a12395d03bbadaad448477b3c56655b
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-12 15:33:08 +0200

    Add routing and separate Main page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,89 +1,21 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
-import NavBar from './components/NavBar'
-import BlogPost from './components/BlogPost'
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import './App.css';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-  },
-});
-
-const categories = [
-  { title: 'Category 1', url: '#' },
-  { title: 'Category 2', url: '#' },
-  { title: 'Category 3', url: '#' },
-];
-
-const featuredPosts = [
-  {
-    title: 'Featured post',
-    date: 'Mar 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Mar 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Featured post',
-    date: 'Mar 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Mar 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Featured post',
-    date: 'Mar 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Mar 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-
-];
+import Main from './components/Main'
+import NewPost from './components/NewPost'
 
 export default function App() {
-
-  const classes = useStyles();
   return (
-    <CssBaseline>
-    <Container maxWidth="lg">
-      <NavBar title="FlotsamBlog" categories={categories}/>
-      <Grid container className={classes.root} spacing={1}>
-            {featuredPosts.map(post => (
-              <BlogPost key={post.title} post={post} />
-            ))}
-      </Grid>
-    </Container>
-    </CssBaseline>
+    <Router>
+      <div>
+        <CssBaseline>
+          <Switch>
+            <Route path="/" exact={true} component={Main}/>
+            <Route path="/newpost" component={NewPost}/>
+          </Switch>
+        </CssBaseline>
+      </div>
+    </Router>
   );
 }
