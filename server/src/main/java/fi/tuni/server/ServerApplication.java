2b784fb2b17cfc80ecea0d8a863abe29a9dbdff0
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-14 16:29:09 +0200

    Add curl commands when starting server.

--- server/src/main/java/fi/tuni/server/ServerApplication.java
+++ server/src/main/java/fi/tuni/server/ServerApplication.java
@@ -16,11 +16,25 @@ public class ServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		System.out.println("AUTHORS");
+		System.out.println("-------------");
 		System.out.println("Lauri Latva-Kyyny & Miko Kauhanen");
+		System.out.println("-------------");
+		System.out.println("CURL COMMANDS");
+		System.out.println("-------------");
+		System.out.println("GET all locations: ");
+		System.out.println("	curl -X GET http://localhost:8080/posts");
+		System.out.println("GET one location: ");
+		System.out.println("	curl -X GET http://localhost:8080/posts/1");
+		System.out.println("DELETE one location: ");
+		System.out.println("	curl -X DELETE http://localhost:8080/posts/1");
+		System.out.println("POST one location: ");
+		System.out.println("	curl -X POST -H \"Content-type: application/json\" -d \"{\\\"title\\\": \"Featured post\", \\\"description\\\": \"Post description\", \\\"date\\\": \"Featured post\", \\\"image\\\": \"https://source.unsplash.com/random\", \\\"imageText\\\": \"Image text\", \\\"category\\\": \"Category 1\"}\" http://localhost:8080/posts/");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		// Insert dummy data as list to database and print them
 		List<BlogPost> blogPosts = new ArrayList();
 		blogPosts.add(new BlogPost("Featured post", "Mar 11", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "https://source.unsplash.com/random", "Image text", "Category 1"));
 		blogPosts.add(new BlogPost("Featured post", "Feb 12", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.", "https://source.unsplash.com/random", "Image text", "Category 2"));
