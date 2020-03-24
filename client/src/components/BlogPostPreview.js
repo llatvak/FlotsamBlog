f9319377da9ee13251fcf08c1437956694f5aa61
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-24 15:14:00 +0200

    Add delete button and event listeners.

--- client/src/components/BlogPostPreview.js
+++ client/src/components/BlogPostPreview.js
@@ -4,6 +4,14 @@ import { Card, Button, Heading } from "react-bulma-components";
 export default function BlogPost(props) {
     const { post } = props;
 
+    function onReadMore(event) {
+        console.log('Read more');
+    }
+
+    function onDelete(event) {
+        console.log('delete');
+    }
+
     return (
         <div>
             <Card>
@@ -11,8 +19,9 @@ export default function BlogPost(props) {
 
                 <Card.Content>
                     <Heading>{post.title}</Heading>
-                    <Heading subtitle="true">{post.body}</Heading>
-                    <Button color="primary">Read more</Button>
+                    <Heading subtitle="true">{post.description}</Heading>
+                    <Button color="primary" onClick={onReadMore}>Read more</Button>
+                    <Button color="danger" onClick={onDelete}>Delete</Button>
                 </Card.Content>
                 <Card.Footer>
                     {post.date}
