2c8d629ffbb355f1495960c6193277b7073c6214
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-24 15:17:38 +0200

    Create useFetch component.

---
+++ client/src/components/useFetch.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+
+export default function useFetch(url) {
+    const [data, setData] = useState([]);
+  
+    async function getData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+    }
+  
+    useEffect(() => {
+      getData();
+    }, []);
+
+  console.log(data);
+  return data;
+}
