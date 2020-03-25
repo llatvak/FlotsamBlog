1a34a92a4dd2b7e3ba2b93618cb58d9a0fc611a6
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-03-25 18:36:08 +0200

    Upload production build.

---
+++ client/build/precache-manifest.9a5fddaa81cca512ea926271f4566eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5522b3edff9d1dc79991eadb1512f185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "521e68d258b3e4e11cce",
+    "url": "/static/css/2.f4da3fac.chunk.css"
+  },
+  {
+    "revision": "521e68d258b3e4e11cce",
+    "url": "/static/js/2.a60dfbb3.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.a60dfbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27532e09ff252992e6a0",
+    "url": "/static/js/main.810c9387.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
