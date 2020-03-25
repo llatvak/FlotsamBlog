67caae0830b94a484dd96111de726e263e8fb7d5
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-03-25 14:10:02 +0200

    Add method for updating posts.

--- server/src/main/java/fi/tuni/server/MyRestController.java
+++ server/src/main/java/fi/tuni/server/MyRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -17,7 +18,7 @@ public class MyRestController {
     BlogPostRepository postRepository;
 
     @RequestMapping(value = "/posts", method = RequestMethod.POST)
-    public ResponseEntity<Void> savePost(@RequestBody BlogPost p, UriComponentsBuilder b) {
+    public ResponseEntity<BlogPost> savePost(@RequestBody BlogPost p, UriComponentsBuilder b) {
 
         this.postRepository.save(p);
 
@@ -25,7 +26,29 @@ public class MyRestController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(uriComponents.toUri());
 
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        return new ResponseEntity<BlogPost>(p, headers, HttpStatus.CREATED);
+    }
+
+    /*@RequestMapping(value = "/posts/{postId}", method = RequestMethod.PUT)
+    public ResponseEntity<BlogPost> updatePost(@RequestBody BlogPost p, UriComponentsBuilder b, @PathVariable int postId) {
+        this.postRepository.save(p);
+        UriComponents uriComponents = b.path("/posts/{id}").buildAndExpand(p.getId());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+        return new ResponseEntity<BlogPost>(p, headers, HttpStatus.OK);
+    }*/
+
+    @PutMapping("posts/{postId}")
+    public ResponseEntity<BlogPost> updatePost(@PathVariable(value = "postId") int postId, @Valid @RequestBody BlogPost postDetails) throws Exception {
+        BlogPost post = postRepository.findById(postId).orElseThrow(() -> new Exception("Not valid id"));
+        post.setTitle(postDetails.getTitle());
+        post.setDate(postDetails.getDate());
+        post.setDescription(postDetails.getDescription());
+        post.setImageText(postDetails.getImageText());
+        post.setImage(postDetails.getImage());
+        post.setCategory(postDetails.getCategory());
+        final BlogPost updatedPost = postRepository.save(post);
+        return ResponseEntity.ok(updatedPost);
     }
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
