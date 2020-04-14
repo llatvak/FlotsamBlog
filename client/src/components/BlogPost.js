8316d77f3de0dc32538d32905f09e74aac6666eb
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-14 17:11:25 +0300

    Add reply option to comments.

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -82,10 +82,10 @@ export default function BlogPost(props) {
         <Content style={body}>
           <Title style={title}>{post.title}</Title>
           <Title style={title} size={6}>{post.category}</Title>
-          <p>
-            {post.date}
-          </p>
-          </Content>
+            <p>
+              {post.date}
+            </p>
+        </Content>
 
           <Image.Container size={'16by9'} style={imageContainer}>
             <Image src={post.imageUrl} style={image}/>
@@ -104,61 +104,56 @@ export default function BlogPost(props) {
           </Control>
 
           <Content hidden={commentsBoxOpen}>
-          <Media style={media}>
-            <Media.Item as="figure" align="left">
-              <FontAwesomeIcon size={'3x'} icon={faUserCircle} />
-            </Media.Item>
-            <Media.Item align="content">
-              <Content>
-                <p>
-                  <strong>John Smith</strong> <small>@johnsmith</small>{' '}
-                  <small>31m</small>
-                  <br />
-                  Very cool post!
-                </p>
-              </Content>
-              <Level breakpoint="mobile">
-                <Level.Item align="left">
-                  <Level.Item as="a">
-                    <Icon size="small">
-                      <FontAwesomeIcon icon={faReply} />
-                    </Icon>
+            <Media style={media}>
+              <Media.Item as="figure" align="left">
+                <FontAwesomeIcon size={'3x'} icon={faUserCircle} />
+              </Media.Item>
+              <Media.Item align="content">
+                <Content>
+                  <p>
+                    <strong>John Smith</strong> <small>@johnsmith</small>{' '}
+                    <small>31m</small>
+                    <br />
+                    Very cool post!
+                  </p>
+                </Content>
+                <Level breakpoint="mobile">
+                  <Level.Item align="left">
+                    <Level.Item>
+                    <small>
+                      <a href="#reply">Reply</a>
+                    </small>
+                    </Level.Item>
+                    <Level.Item as="a">
+                      <Icon size="small">
+                        <FontAwesomeIcon color={heartIconColor} icon={faHeart} onClick={() => handleHeartIconClick()} />
+                      </Icon>
+                      <Content>
+                        <small>{heartAmount}</small>
+                      </Content>
+                    </Level.Item>
                   </Level.Item>
-                  <Level.Item as="a">
-                    <Icon size="small">
-                      <FontAwesomeIcon icon={faRetweet} />
-                    </Icon>
-                  </Level.Item>
-                  <Level.Item as="a">
-                    <Icon size="small">
-                      <FontAwesomeIcon color={heartIconColor} icon={faHeart} onClick={() => handleHeartIconClick()} />
-                    </Icon>
-                    <Content>
-                      <small>{heartAmount}</small>
-                    </Content>
-                  </Level.Item>
-                </Level.Item>
-              </Level>
-            </Media.Item>
-          </Media>
-          
-          <Media as="article">
-            <Media.Item align="left">
-              <FontAwesomeIcon size={'3x'} icon={faUserCircle} />
-            </Media.Item>
-            <Media.Item align="content">
-              <Field>
-                <Control as="p">
-                  <Textarea placeholder="Add a comment..." />
-                </Control>
-              </Field>
-              <Field>
-                <Control as="p">
-                  <Button>Post comment</Button>
-                </Control>
-              </Field>
-            </Media.Item>
-          </Media>
+                </Level>
+              </Media.Item>
+            </Media>
+    
+            <Media as="article">
+              <Media.Item align="left">
+                <FontAwesomeIcon size={'3x'} icon={faUserCircle} />
+              </Media.Item>
+              <Media.Item align="content">
+                <Field>
+                  <Control as="p">
+                    <Textarea placeholder="Add a comment..." />
+                  </Control>
+                </Field>
+                <Field>
+                  <Control as="p">
+                    <Button>Post comment</Button>
+                  </Control>
+                </Field>
+              </Media.Item>
+            </Media>
           </Content>
 
     </Box>
