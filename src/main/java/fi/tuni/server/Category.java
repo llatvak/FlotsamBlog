05028b4b7bb48186e65c723b915b562e4b1ec448
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-04-14 20:34:38 +0300

    Create entity Category.

---
+++ src/main/java/fi/tuni/server/Category.java
@@ -0,0 +1,38 @@
+package fi.tuni.server;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+
+    public Category(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", title='" + title + '\''+
+                '}';
+    }
+}
