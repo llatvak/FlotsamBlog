c59534099e51afff4d5c30b8343484c87aa09492
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-16 09:27:57 +0300

    Modify disabling cross-origin.

--- src/main/java/fi/tuni/server/MyConfiguration.java
+++ src/main/java/fi/tuni/server/MyConfiguration.java
@@ -13,10 +13,7 @@ public class MyConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                .allowCredentials(true)
+                .allowedMethods("*")
         ;
     }
 }
