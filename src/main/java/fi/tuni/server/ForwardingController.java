ef4b0be9d3d2011a1445fa93cc78b760abaac3fa
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-16 18:11:47 +0300

    Add forwarding controller.

---
+++ src/main/java/fi/tuni/server/ForwardingController.java
@@ -0,0 +1,12 @@
+package fi.tuni.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardingController {
+    @RequestMapping("/**/{path:[^\\.]+}")
+    public String forward() {
+        return "forward:/";
+    }
+}
