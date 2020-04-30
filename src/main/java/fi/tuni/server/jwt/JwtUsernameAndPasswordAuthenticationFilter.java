f5b3b20a4637f0d5a72680275e2bedd85d61c1c8
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-30 12:29:37 +0300

    Modify AppSecurityConfig and add new class for password encoding.

--- src/main/java/fi/tuni/server/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/fi/tuni/server/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -14,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Date;
 import java.time.LocalDate;
 
 // Verify credentials sent by client
--- src/main/java/fi/tuni/server/security/AppSecurityConfig.java
+++ src/main/java/fi/tuni/server/security/AppSecurityConfig.java
@@ -1,9 +1,11 @@
 package fi.tuni.server.security;
 
+import fi.tuni.server.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,13 +15,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AppSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http
+        http
+                .csrf().disable()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
-                .antMatchers("/dashboard").hasAuthority("ADMIN")
-                .antMatchers("/**").hasAuthority("ADMIN");
-        */
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole("ADMIN");
     }
 
 
@@ -29,12 +40,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     {
         authentication.inMemoryAuthentication()
                 .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
+                .password(passwordEncoder.encode("admin"))
                 .authorities("ADMIN");
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
