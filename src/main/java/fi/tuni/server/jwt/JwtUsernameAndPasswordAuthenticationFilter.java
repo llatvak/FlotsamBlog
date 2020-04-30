e0654f65f6f223c9b1e672a6fb585f3986ca272c
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-30 11:55:11 +0300

    Verify credentials sent by client.

--- src/main/java/fi/tuni/server/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/fi/tuni/server/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,18 +1,41 @@
 package fi.tuni.server.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-// Verify credentials
+// Verify credentials sent by client
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response) throws AuthenticationException {
-
-        return super.attemptAuthentication(request, response);
+        try {
+            // Object mapper reads value from input stream and puts it into UsernameAndPasswordAuthentication class
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+            // Create authentication with principle and credentials and authenticate does username exist and password is correct
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+            );
+            Authentication authenticate = authenticationManager.authenticate(authentication);
+            return authenticate;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
