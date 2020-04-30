20caa1494c435d5f572f57ce8ea020e3269b4553
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-04-30 12:49:59 +0300

    Add admin user.

--- src/main/java/fi/tuni/server/security/AppSecurityConfig.java
+++ src/main/java/fi/tuni/server/security/AppSecurityConfig.java
@@ -4,14 +4,15 @@ import fi.tuni.server.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -37,14 +38,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").hasRole("ADMIN");
     }
 
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authentication)
-            throws Exception
-    {
-        authentication.inMemoryAuthentication()
-                .withUser("admin")
+    // Retrieve user from database
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin1 = User.builder()
+                .username("admin")
                 .password(passwordEncoder.encode("admin"))
-                .authorities("ADMIN");
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(
+                admin1
+        );
     }
 }
