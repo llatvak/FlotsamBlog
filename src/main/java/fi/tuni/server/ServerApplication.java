ebce0e9f341d4900305c8d4489ede7eab480f6d1
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-04 09:12:24 +0300

    Add new blog posts and delete unused properties.

--- src/main/java/fi/tuni/server/ServerApplication.java
+++ src/main/java/fi/tuni/server/ServerApplication.java
@@ -71,11 +71,49 @@ public class ServerApplication implements CommandLineRunner {
                                         "https://3.bp.blogspot.com/-Z7zdfBjgT7s/Xn5KK2tHgSI/AAAAAAAAOx8/T6bReu6ky4E8gnTOSsSZJDbTOs4W1HbzQCLcBGAsYHQ/s1600/Android%2BDevelopers%2BBlog%2B%25281200x600%2529.png", 
                                         "Android"
                                     );
+        BlogPost post4 = new BlogPost("USPTO: AIs cannot be credited as inventors...",
+                                        "US Patent Office has ruled that an AI cannot be legally credited as an inventor...",
+                                        "The US Patent and Trademark Office (USPTO) has ruled that an AI cannot be legally credited as an inventor. AI will assist us mere humans in coming up with new innovations in the years to come. However, the USPTO will not let them take the credit. The USPTO has rejected two early filings of inventions credited to an AI system called DABUS which was created by Stephen Thaler. \n\n" +
+                                                "DABUS invented two devices; a shape-shifting food container, and a new type of emergency flashlight. The filings were submitted by the Artificial Inventor Project (AIP) last year. AIP’s lawyers argued that Thaler is an expert in building AI systems like DABUS but has no experience in consumer goods and would not have created them himself.\n\n" +
+                                                "The USPTO concluded that “only natural persons may be named as an inventor in a patent application,” under the current law. Similar applications by the AIP in the UK and EU were rejected along the same lines by their respective patent authorities.\n\n" +
+                                                "“If I teach my Ph.D. student and they go on to make a final complex idea, that doesn’t make me an inventor on their patent, so it shouldn’t with a machine,” Ryan Abbott, a professor at the University of Surrey who led a group of legal experts in the AI patent project, told the Wall Street Journal last year. \n\n" +
+                                                "The case over whether only humans should hold such rights has similarities to the infamous monkey selfie saga where PETA argued that a monkey could own the copyright to a selfie. The US Copyright Office also ruled in that instance that only photographs taken by humans can be copyrighted and PETA’s case was subsequently dismissed.",
+                                        "30.4.2020",
+                                        "https://artificialintelligence-news.com/2020/04/30/us-patent-office-ai-credited-inventor/",
+                                        "https://www.wpp.com/-/media/project/wpp/images/voices/ai-and-the-personalised-marketing-approach-of-the-future-min.jpg?width=1600&height=900",
+                                        "AI"
+        );
+        BlogPost post5 = new BlogPost("Coronavirus: Graduates struggling for job",
+                                        "On the cusp of entering the world of work, young people are seeing uncertainty engulf their employment prospects...",
+                                        "On the cusp of entering the world of work, young people are seeing uncertainty engulf their employment prospects. Elise Lauriot-Prévost, from Lyon, France, has just finished a master's degree.\n\n " +
+                                                "After sending 50 job applications, she has received little encouragement. Especially right now, you are getting the negative responses and you are getting the responses 'there are no new job adverts anyway', said the 22-year-old.\n\n" +
+                                                "And even when it's not an outright rejection, there's still no good news. You are getting the responses, 'thanks for applying, however, because of coronavirus, we are suspending our job hunting and we will let you know when things have moved on', but none of us know when that will be, she said.\n\n\" + " +
+                                                "Prospects for getting a paid traineeship or a short duration contract are also slim.At this point I (wonder) what field I can even work in. I do not even have anything to do to hold me over until a job in my field, which is very competitive, opens up again. I can not do babysitting, I can not do all the jobs that I usually do, which I have done during school, to be able to earn money, laments Elise.\n\n" +
+                                                "Before the pandemic, the average rate for youth unemployment in the EU stood at 15 per cent. In southern EU countries, some of the hardest hit by COVID-19, the figure stood at 30 per cent.",
+                                        "29.4.2020",
+                                        "https://www.euronews.com/2020/04/29/coronavirus-graduates-struggling-for-job-leads-amid-covid-19-lockdown-uncertainty",
+                                        "https://www.albawaba.com/sites/default/files/styles/de2e_standard/public/im_new/dima/Unemployment_shutterstock_Apr15.jpg?h=b3660f0d&itok=s9eK1KUg",
+                                        "Work"
+        );
+        BlogPost post6 = new BlogPost("GitHub makes its core tools free for all development teams",
+                                        "GitHub has given developers a welcome helping hand during these difficult times by making its core tools free...",
+                                        "GitHub has given developers a welcome helping hand during these difficult times by making its core tools free. Organisations can now make private repositories without paying any fees. Previously, even small teams would’ve had to pay for a premium membership starting from $7/month.\n\n + " +
+                                                "Last year, GitHub made private repositories available for free users but with restrictions in areas such as the number of collaborators; which hindered teams. Unlimited collaborators and private repositories are now included in the free tier. Teams can now also access 2,000 GitHub “action minutes” per month at no additional cost.\n\n" +
+                                                "GitHub Actions enable applications to automate individual tasks. Developers can combine their own series of tasks to create Actions, or use ones created by the community. 500MB of GitHub Packages storage is also now included in the free tier. The free tier is great for many small teams with smaller budgets, but larger teams will likely need a few more features.\n\n" +
+                                                "GitHub has reduced its former “Team” premium tier from $9/month to $4/month. For the additional fee, development teams can access 3,000 Actions minutes and 2GB of GitHub Packages storage. While the COVID-19 pandemic likely prompted a quicker rollout, GitHub CEO Nat Friedman says the company has been considering the change for the last 18 months.",
+                                        "15.4.2020",
+                                        "https://developer-tech.com/news/2020/apr/15/github-core-tools-free-development-teams/",
+                                        "https://images.wallpaperscraft.com/image/code_text_colorful_140555_1600x900.jpg",
+                                        "Programming"
+        );
 
         // Add dummy BlogPosts to list and save to crud repository
         blogPosts.add(post1);
         blogPosts.add(post2);
         blogPosts.add(post3);
+        blogPosts.add(post4);
+        blogPosts.add(post5);
+        blogPosts.add(post6);
         postRepository.saveAll(blogPosts);
 
         // Add dummy Categories to list and save to crud repository
@@ -134,7 +172,7 @@ public class ServerApplication implements CommandLineRunner {
         System.out.println("	curl -X PUT -H \"Content-type: application/json\" -d \"{\\\"title\\\": \\\"Updated Category\\\"}\" http://localhost:8080/api/categories/1");
         System.out.println("-------------");
         System.out.println("Dashboard admin username and password: ");
-        System.out.println("Username: admin, Password: admin");
+        System.out.println("    Username: admin, Password: admin");
 
         //postRepository.findAll().forEach(System.out::println);
     }
