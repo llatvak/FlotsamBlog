a2d25b0641d1a1cd48e1164de3a2f01d3de417d0
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-06 06:38:23 +0300

    Fix login page responsiveness.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,46 +1,31 @@
 import React, { useState } from "react";
-import { Field, Input, Box, Control, Button, Title, Label } from "rbx";
+import { Field, Input, Box, Control, Button, Title, Label, Container } from "rbx";
 import { Redirect } from "react-router-dom";
 import axios from 'axios';
 import { useAuth } from "../context/auth"
 
+const container = {
+  'padding': '16px'
+};
 const box = {
   'margin': 'auto',
-  'padding': '50px',
-  'marginTop': '150px',
-  'maxWidth': '40%'
-};
-
-const textField = {
-
+  'padding': '30px',
+  'marginTop': '100px',
+  'width': '320px'
 };
 
 export default function Login(props) {
-  const [post, setPost] = useState([])
   const [isLoggedIn, setLoggedIn] = useState(false)
   const [passwordRef, setPasswordRef] = useState('')
   const [usernameRef, setUsernameRef] = useState('')
   const { setAuthTokens } = useAuth();
 
-  let url = process.env.REACT_APP_POSTS_API_URL_PROD;
   let loginUrl = process.env.REACT_APP_LOGIN_URL_DEVEL;
 
   if(process.env.NODE_ENV !== 'production') {
-      url = process.env.REACT_APP_POSTS_API_URL_DEVEL;
+    // needs production login url?
+    // loginUrl = process.env.REACT_APP_LOGIN_URL_PROD;
   }
-
-/*
-   useEffect(() => {
-    axios
-     .get(url)
-     .then(response => {
-       setPost(response.data);
-       console.log(response);
-     }).catch(error => {
-       alert(`${error}`)
-   })
- }, []) 
- */
 
   const postLogin = () => {
     let usernameValue = usernameRef.value
@@ -63,34 +48,43 @@ export default function Login(props) {
     })
   }
   
+  const handleKeyPressLogin = (event) => {
+    if(event.key === 'Enter'){
+      console.log('enter')
+      postLogin();
+    }
+  }
+
   if(isLoggedIn) {
     return <Redirect to="/dashboard" />
   }
 
   return (
     <div>
+    <Container breakpoint="mobile" style={container}>
       <Box style={box}>
       <Title>Login</Title>
-        <Field style={textField}>
+        <Field align="centered">
           <Label>Username</Label>
           <Control>
-            <Input type="text" placeholder="Username" ref={(username) => {setUsernameRef(username)}}/>
+            <Input type="text" placeholder="Username" ref={(username) => {setUsernameRef(username)}} onKeyDown={handleKeyPressLogin}/>
           </Control>
         </Field>
 
-        <Field style={textField}>
+        <Field align="centered">
           <Label>Password</Label>
           <Control>
-            <Input type="password" placeholder="Password" ref={(password) => {setPasswordRef(password)}} />
+            <Input type="password" placeholder="Password" ref={(password) => {setPasswordRef(password)}} onKeyDown={handleKeyPressLogin}/>
           </Control>
         </Field>
 
         <Field>
           <Control>
-            <Button onClick={postLogin} color="success">Login</Button>
+            <Button onClick={postLogin} color="primary">Login</Button>
           </Control>
         </Field>
       </Box>
+      </Container>
     </div>
   );
 }
